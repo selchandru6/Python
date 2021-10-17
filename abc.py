efe7544bb2c8d5e23423a6148171ac25494af6e7
chandransellappan <sel.chandru@gmail.com>
2021-10-17 13:23:28 -0500

    Updated code

--- abc.py
+++ abc.py
@@ -1,53 +1,7 @@
-<<<<<<< HEAD
-total 8
--rw-r--r--  1 chandransellappan  staff  107 Oct 17 12:25 README.md
--rw-r--r--  1 chandransellappan  staff    0 Oct 17 12:29 abc.py
-inertert
-et
-er
-te
-er
-tetertrhiuyhtriyhtriyhturyrtuy
-
-
-rtyrtyrytry
-
-
-dsfsf
-sd
-fsfds
-
-fs
-fds
-f
-sd
-fsd
-fds
-f
-s
-f
-sfd	
-2342342
-2
-3423
-423
-42
-4
-
-424
-42
-4
-234
-2
-
-
-rtyryrtytiriyrioy
-=======
 import calendar  
 # Enter the month and year  
 yy = int(input("Enter year: "))  
 mm = int(input("Enter month: "))  
   
 # display the calendar  
-print(calendar.month(yy,mm))  
->>>>>>> 0da459c13130c9521752e6b1f124706d0ae84c53
+print(calendar.month(yy,mm)) 
