0da459c13130c9521752e6b1f124706d0ae84c53
Chandran Sellappan <sel.chandru@gmail.com>
2021-10-17 12:51:54 -0500

    Update abc.py
    Updated with python program.

--- abc.py
+++ abc.py
@@ -1,18 +1,7 @@
-total 8
--rw-r--r--  1 chandransellappan  staff  107 Oct 17 12:25 README.md
--rw-r--r--  1 chandransellappan  staff    0 Oct 17 12:29 abc.py
-inertert
-et
-er
-te
-er
-tetertrhiuyhtriyhtriyhturyrtuy
-
-
-rtyrtyrytry
-
-
-
-
-
-rtyryrtytiriyrioy
+import calendar  
+# Enter the month and year  
+yy = int(input("Enter year: "))  
+mm = int(input("Enter month: "))  
+  
+# display the calendar  
+print(calendar.month(yy,mm))  
